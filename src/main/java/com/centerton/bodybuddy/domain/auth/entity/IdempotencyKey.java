f0b922060198cae7f9d5911899329226e77383ce
jmyoo0512 <jm030512@gmail.com>
2026-08-11 06:31:00 +0900

    feat: 중복 방지 키 엔티티 구현

---
+++ src/main/java/com/centerton/bodybuddy/domain/auth/entity/IdempotencyKey.java
@@ -0,0 +1,24 @@
+package com.centerton.bodybuddy.domain.auth.entity;
+
+import com.centerton.bodybuddy.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "idempotency_keys")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class IdempotencyKey extends BaseEntity {
+
+    @Id
+    @Column(name = "idempotency_key", length = 64)
+    private String idempotencyKey;
+
+    @Column(name = "user_id", nullable = false, length = 64)
+    private String userId;
+}
