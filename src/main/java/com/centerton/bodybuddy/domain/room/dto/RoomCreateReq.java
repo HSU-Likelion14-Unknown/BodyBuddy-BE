31e27c9558b27f3d866ada59400e95f961307092
jmyoo0512 <jm030512@gmail.com>
2026-08-15 01:58:52 +0900

    feat: 방 이름 길이 검증 추가

--- src/main/java/com/centerton/bodybuddy/domain/room/dto/RoomCreateReq.java
+++ src/main/java/com/centerton/bodybuddy/domain/room/dto/RoomCreateReq.java
@@ -1,6 +1,7 @@
 package com.centerton.bodybuddy.domain.room.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 public class RoomCreateReq {
 
     @NotBlank(message = "방 이름을 확인해주세요.")
+    @Size(min = 1, max = 10, message = "방 이름 길이를 확인해주세요.")
     private String roomName;
 }
